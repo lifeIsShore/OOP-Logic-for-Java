a63ed1da88451bceb2ba75af7601dda647891814
Ahmet Yagmur <ahmtyagmur03@gmail.com>
2023-04-28 13:45:15 +0200

    Object exercises

---
+++ object/animal.java
@@ -0,0 +1,14 @@
+package object;
+
+public class animal {
+    private int numLegs;
+
+    public animal(int numlegs) {
+        this.numLegs = numlegs;
+    }
+
+    public int getLegs() {
+        return numLegs;
+    }
+
+}
---
+++ object/ob1.java
@@ -0,0 +1,8 @@
+package object;
+
+public class ob1 {
+    public static void main(String[] args) {
+        animal anml = new animal(4);
+        System.out.println(anml.getLegs());
+    }
+}
